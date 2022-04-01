9fd35f6203abff7fb7af0b3849f58e7c85d45680
ZnoGouDj <znogoud3@gmail.com>
2022-04-01 12:23:24 +0200

    Fix errors to launch

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ import {
   BALL_STARTX,
   BALL_STARTY,
 } from './setup';
+// Helpers
+import { createBricks } from './helpers';
 
 let gameOver = false;
 let score = 0;
@@ -30,9 +32,27 @@ function setGameWin(view: CanvasView) {
   gameOver = false;
 }
 
-function gameLoop(view: CanvasView, bricks: Brick[], paddle: Paddle, ball: Ball);
+function gameLoop(
+  view: CanvasView,
+  bricks: Brick[]
+  // paddle: Paddle, ball: Ball
+) {
+  view.clear();
+  view.drawBricks(bricks);
 
-function startGame(view: CanvasView) {}
+  requestAnimationFrame(() => gameLoop(view, bricks));
+}
+
+function startGame(view: CanvasView) {
+  // Reset displays
+  score = 0;
+  view.drawInfo('');
+  view.drawScore(0);
+  // Create all bricks
+  const bricks = createBricks();
+
+  gameLoop(view, bricks);
+}
 
 // Create a new view
 const view = new CanvasView('#playField');
